cc75a59df1951ce844c6d3207b41ab09e95689c9
MT121201 <113223902+MT121201@users.noreply.github.com>
2023-06-22 10:27:21 +0700

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -1,27 +1,25 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 
-# Load the customer data from CSV file
-df = pd.read_csv('d7.csv')
+# Generate random data using make_blobs
+X, y = make_blobs(n_samples=300, centers=3, random_state=42)
 
-# Extract the features for clustering
-X = df.iloc[:, 0:3].values
-
-# Perform k-means++ clustering with 2 clusters
-k = 2
-kmeans = KMeans(n_clusters=k, init='k-means++', random_state=42)
+# Apply k-means clustering with k-means++ initialization
+kmeans = KMeans(n_clusters=3, init='k-means++')
 kmeans.fit(X)
 
-# Get the cluster labels for each data point
+# Get cluster labels
 labels = kmeans.labels_
 
-# Add the cluster labels as a new column in the dataframe
-df['Cluster'] = labels
+# Get cluster centers
+centers = kmeans.cluster_centers_
 
-# Visualize the clustered data
-plt.scatter(df['Annual Income ($)'], df['Spending Score (0-100)'], c=labels, cmap='viridis')
-plt.xlabel('Annual Income ($)')
-plt.ylabel('Spending Score (0-100)')
-plt.title('K-Means++ Clustering')
+# Plot the data points with color-coded clusters
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
+plt.scatter(centers[:, 0], centers[:, 1], marker='x', color='red')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('K-means Clustering with K-means++ Initialization')
 plt.show()

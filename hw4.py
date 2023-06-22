3af414a090bc76847b94a812297b011014bd8e98
MT121201 <113223902+MT121201@users.noreply.github.com>
2023-06-22 09:56:44 +0700

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -2,13 +2,13 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-
+import matplotlib.pyplot as plt
 # Read the dataset from the CSV file
-df = pd.read_csv('d4.csv')
+df = pd.read_csv('sea_hotel.csv')
 
 # Split the dataset into input features (X) and target variable (y)
-X = df.drop('Sales Revenue', axis=1)
-y = df['Sales Revenue']
+X = df.drop('Hotel Prices', axis=1)
+y = df['Hotel Prices']
 X = X.values
 y = y.values
 
@@ -24,11 +24,15 @@ model.fit(X_train, y_train)
 # Make predictions on the test set
 y_pred = model.predict(X_test)
 
+# Compare different Ground_truth and Predict
+plt.scatter(y_test, y_pred)
+plt.xlabel("Prices: $Y_i$")
+plt.ylabel("Predicted prices: $\hat{Y}_i$")
+plt.title("Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$")
+plt.show()
+
 # Calculate the mean squared error
 mse = mean_squared_error(y_test, y_pred)
 print(f"Mean Squared Error: {mse}")
 
-# Predict sales revenue for a new input
-new_input = [[1000, 500, 200]]  # Example input values
-predicted_revenue = model.predict(new_input)
-print(f"Predicted Sales Revenue: {predicted_revenue}")
+
